2e34df9b3841ff5930682a1c63e9464f5690d7ce
Code First! <41821541+Liubomyr17@users.noreply.github.com>
2020-02-29 11:06:04 +0200

    Add files via upload

---
+++ JavaCore 1116 Inheritance Chain/src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+/*
+
+Level 11 Lection 11. Inheritance Chain
+Arrange correctly the “chain of inheritance” in the classes: Pet (pet), Cat (cat), Dog (dog).
+
+Requirements:
+1. In the class Solution there must be a public class Pet (pet).
+2. In the class Solution there must be a public class Cat (cat).
+3. The Solution class must have a public Dog class.
+4. The Cat class must be inherited from the Pet class.
+5. The class Dog must be inherited from the class Pet.
+
+ */
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystemException;
+import java.nio.file.attribute.AclFileAttributeView;
+import java.rmi.RemoteException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public class Pet {
+    }
+
+    public class Cat extends Pet {
+    }
+
+    public class Dog extends Pet {
+    }
+}
